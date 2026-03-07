e5df56a88e67873f238fb3336b2ac7264dfcd267
Yash Rajput <yrjpoot648@gmail.com>
2026-03-07 18:34:33 +0530

    fix bigquery private key newlines for render

--- backend/src/config/bigquery.js
+++ backend/src/config/bigquery.js
@@ -1,8 +1,13 @@
 const { BigQuery } = require('@google-cloud/bigquery');
 
+const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS);
+if (credentials.private_key) {
+  credentials.private_key = credentials.private_key.replace(/\\n/g, '\n');
+}
+
 const bigquery = new BigQuery({
   projectId: process.env.BIGQUERY_PROJECT_ID,
-  credentials: JSON.parse(process.env.GOOGLE_CREDENTIALS)
+  credentials: credentials
 });
 
 module.exports = bigquery;
